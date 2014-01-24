7bb7672884db1beb1e830af33184c714ee66fbf1
Micah Sidebottom <Sidebottom1@gmail.com>
2014-01-24 21:08:45 +0000

    Bug central ahoy

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ NkuRails::Application.routes.draw do
 
   resources :posts
     resources :comments
-  #post GET    /posts/:id(.:format)      posts#show
-  #posts GET    /posts(.:format)          posts#index
-  #DELETE /posts/:id(.:format)      posts#destroy
-  
-  #Tutorial thingy seems to want me to add all of the above, but
-  # none of them seem to be formatted in a way the editor 'likes' so to speak.
   
   root to: "welcome#index"
 end
