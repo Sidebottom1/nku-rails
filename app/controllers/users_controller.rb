2a016fec1fb7b357e368d5f36cbed8a3d4528e71
Micah Sidebottom <Sidebottom1@nku.edu>
2014-01-31 02:43:42 +0000

    More extensive additions, in process of figuring out what all is left to do in views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,51 @@
 class UsersController < ApplicationController
+  
   def new
+    @maker = Maker.new
   end
+  
+  def index
+    @maker = Maker.all
+  end
+  
+  def create
+    @maker = Maker.new(maker_params)
+    
+    if @maker.save
+      redirect_to @maker
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    @maker = Maker.find(params[:id])
+    @maker.destroy
+
+    redirect_to maker_path
+  end
+    
+  def show
+    @maker = Maker.find(params[:id])
+  end
+  
+  def edit
+    @maker = Maker.find(params[:id])
+  end
+    
+  def update
+    @maker = Maker.find(params[:id])
+    
+    if @maker.update(params[:maker].permit(:name, :nickname, :email, :imageURL))
+      redirect_to @maker
+    else
+      render 'edit'
+    end
+  end
+    
+  private
+    def maker_params
+      params.require(:maker).permit(:name, :nickname, :email, :imageURL)
+    end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140124174520) do
+ActiveRecord::Schema.define(version: 20140131023133) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20140124174520) do
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "makers", force: true do |t|
+    t.string   "name"
+    t.string   "nickname"
+    t.string   "email"
+    t.string   "imageURL"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
