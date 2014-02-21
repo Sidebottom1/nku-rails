de01b9442606e4567094ffd3c6bdda4cc81197d2
Micah Sidebottom <Sidebottom1@nku.edu>
2014-02-21 20:22:13 +0000

    mandatory commit

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,8 +1,8 @@
 class Attendance < ActiveRecord::Base
   belongs_to :user
-  validates_presence_of :seat, message: "You must enter a seat number"
-  validates_numericality_of :seat, less_than: 5, greater_than_or_equal_to: 1, message: "Your seat number must be 1 - 4"
-  validates_numericality_of :seat, only_integer: true, message: "Your seat number must be a number"
-  validates :user, :uniqueness => true
+  validates_presence_of :seat, message: "number input required"
+  validates_numericality_of :seat, less_than: 5, greater_than_or_equal_to: 1, message: "number must be 1 - 4"
+  validates_numericality_of :seat, only_integer: true, message: "number must be a number"
+  validates :user, :uniqueness => { :message => "already signed in" }
   
 end
