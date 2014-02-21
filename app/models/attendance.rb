ede94940054a34795f3f46b72f48cb0ced0cbfc0
Micah Sidebottom <Sidebottom1@nku.edu>
2014-02-21 21:21:25 +0000

    safety commit after nitrous outage

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,6 +3,6 @@ class Attendance < ActiveRecord::Base
   validates_presence_of :seat, message: "number input required"
   validates_numericality_of :seat, less_than: 5, greater_than_or_equal_to: 1, message: "number must be 1 - 4"
   validates_numericality_of :seat, only_integer: true, message: "number must be a number"
-  validates :user, :uniqueness => { :message => "already signed in" }
+  
   
 end
